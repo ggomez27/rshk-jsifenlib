dc5a0787f57a43ee289ac36aa3e61e8f198d97d5
Gabriel Gomez <gabrielgzb97@gmail.com>
2023-04-11 21:55:04 -0400

    Agregado el código para probar las interacciones de la Librería con el Sifen.

---
+++ src/main/java/com/roshka/sifen/SifenExampleApp.java
@@ -0,0 +1,204 @@
+package com.roshka.sifen;
+
+import com.roshka.sifen.core.SifenConfig;
+import com.roshka.sifen.core.beans.DocumentoElectronico;
+import com.roshka.sifen.core.beans.EventosDE;
+import com.roshka.sifen.core.beans.ValidezFirmaDigital;
+import com.roshka.sifen.core.beans.response.*;
+import com.roshka.sifen.core.exceptions.SifenException;
+import com.roshka.sifen.core.fields.response.TgResProc;
+import com.roshka.sifen.core.fields.response.TxProtDe;
+import com.roshka.sifen.core.fields.response.batch.TgResProcLote;
+import com.roshka.sifen.core.fields.response.event.TgResProcEVe;
+import com.roshka.sifen.core.fields.response.ruc.TxContRuc;
+
+import java.util.Collections;
+
+public class SifenExampleApp {
+    public static void main(String[] args) throws SifenException {
+
+        // Se carga la Configuración del SIFEN
+        SifenConfig config = SifenConfig.cargarConfiguracion("C:\\.AoC\\sifen.properties");
+        Sifen.setSifenConfig(config);
+
+        // Consulta de RUC
+        System.out.println(pruebaConsultaRUC("80089752"));
+
+        // Creación de una Factura
+        DocumentoElectronico DE = SifenExampleHelper.cargarDE("0000016");
+        System.out.println("CDC del Documento Electrónico -> " + DE.obtenerCDC());
+
+        // Se verifica que la Factura sea valida
+        ValidezFirmaDigital validez = Sifen.validarFirmaDEDesdeXml(DE.generarXml());
+        if (validez.isValido()) {
+            System.out.println("El documento es valido");
+
+            // Emisión de una Factura
+            // System.out.println(pruebaRecepcionDE(DE));
+
+            // Emisión Lote de DE
+            System.out.println(pruebaRecepcionYConsultaLoteDE(DE));
+
+            // Anulación de la Factura recien Emitida
+            System.out.println(pruebaAnularFactura(DE));
+        } else System.out.println(validez.getMotivoInvalidez());
+
+    }
+
+    /**
+     * Metodo que realiza una consulta de RUC al Sifen y devuelve como resultado una cadena con los datos
+     * recibidos de la consulta.
+     *
+     * @param ruc El RUC que se desea consultar
+     * @return Una cadena formateada con la respuesta de la Consulta
+     * @throws SifenException Si la configuración de Sifen no fue establecida o, si algún dato necesario para la
+     *                        consulta no pudo ser encontrado o, si la consulta no pudo ser realizada.
+     */
+    public static String pruebaConsultaRUC(String ruc) throws SifenException {
+
+        RespuestaConsultaRUC respuesta = Sifen.consultaRUC(ruc);
+
+        //Un dCodRes con código 0500 indica que el RUC no existe (capítulo 12.3.5.3, Manual Técnico de Sifen)
+        if (respuesta.getdCodRes().equals("0500"))
+            return String.format("El RUC %s no existe", ruc);
+
+        //Un dCodRes con código 0501 indica que el RUC no tiene permiso para utilizar el WS. (capítulo 12.3.5.3, Manual Técnico de Sifen)
+        if (respuesta.getdCodRes().equals("0501"))
+            return "El RUC no tiene permiso para utilizar el WS";
+
+        //Caso contrario, dCodRes tendrá valor 0502, y existirá un xContRuc con los datos del resultado (Schema XML 16)
+        TxContRuc xContRuc = respuesta.getxContRUC();
+
+        return String.format("Consulta de RUC:\n" +
+                        "\tRUC: %s\n" +
+                        "\tRazon Social: %s\n" +
+                        "\tEstado: %s\n" +
+                        "\tEs facturador: %s\n" +
+                        "Fin Consulta de RUC",
+                xContRuc.getdRUCCons(),
+                xContRuc.getdRazCons(),
+                xContRuc.getdCodEstCons(),
+                xContRuc.getdRUCFactElec());
+    }
+
+    /**
+     * Método que envía un DE al Sifen, y devuelve una cadena con los datos recibidos en la respuesta de la
+     * Recepción del DE.
+     * <p>
+     * Este método hace uso del método Sifen.recepcionDE(de) de la librería RSHK-JSIFENLIB
+     *
+     * @param de El Documento Electrónico a enviar.
+     * @return Una cadena formateada con el resultado de la Recepción de DE realizada
+     * @throws SifenException Una cadena formateada con el resultado de la Recepción y Consulta de Lote de DE realizadas (en el caso
+     *                        de la última, sólo si se pudo realizar la consulta)
+     */
+    public static String pruebaRecepcionDE(DocumentoElectronico de) throws SifenException {
+
+        RespuestaRecepcionDE respuesta = Sifen.recepcionDE(de);
+
+        // Protocolo de Procesamiento del DE (Schema XML 3)
+        TxProtDe xProtDe = respuesta.getxProtDE();
+
+        String resultado = String.format("Recepción de Factura\n" +
+                        "\tCodigo de Estado: %d\n" +
+                        "\tEstado Resultado: %s\n" +
+                        "\tRespuestas de Procesamiento:\n",
+                respuesta.getCodigoEstado(),
+                xProtDe.getdEstRes());
+
+        // Por cada Respuesta de Procesamiento en xProtDe
+        for (TgResProc gResProc : xProtDe.getgResProc()) {
+
+            String subString = String.format("\t* Cod %s: %s\n", gResProc.getdCodRes(), gResProc.getdMsgRes());
+            resultado = resultado.concat(subString);
+        }
+
+        return resultado.concat("Fin Recepción de Factura");
+    }
+
+    /**
+     * Método que envía un Evento de Lote de DE al Sifen, y si la recepción fue exitosa y se recibe el número de Lote,
+     * realiza una Consulta de Lote de DE con dicho número. Devuelve una cadena con los datos recibidos en las
+     * respuestas de Recepción y Consulta de Lote de DE (en el caso del último, solo si se pudo realizar la consulta)
+     * <p>
+     * Este método hace uso del método Sifen.recepcionLoteDE(deList) y Sifen.consultaLoteDE(nroLote)
+     * de la librería RSHK-JSIFENLIB
+     *
+     * @param de El Documento Electrónico a enviar en el lote.
+     * @return Una cadena formateada con el resultado de la Recepción y Consulta de Lote de DE realizadas (en el caso
+     * de la última, sólo si se pudo realizar la consulta)
+     * @throws SifenException Una cadena formateada con el resultado de la Recepción y Consulta de Lote de DE realizadas (en el caso
+     *                        de la última, sólo si se pudo realizar la consulta)
+     */
+    public static String pruebaRecepcionYConsultaLoteDE(DocumentoElectronico de) throws SifenException {
+        RespuestaRecepcionLoteDE respuestaRecepcion = Sifen.recepcionLoteDE(Collections.singletonList(de));
+        String resultado = String.format("Recepción de Lote DE\n" +
+                        "\tCodigo Resultado: %s\n" +
+                        "\tNumero de Lote: %s\n",
+                respuestaRecepcion.getdCodRes(),
+                respuestaRecepcion.getdProtConsLote());
+        //dCodRes puede ser 300 o 301 para recibido con exito o no encolado para procesamiento, respectivamente
+        //dProtConsLote es el numero de lote recibido, generado solo si dCodRes=0300
+
+        if (respuestaRecepcion.getdCodRes().equals("0301"))
+            return resultado.concat("\tLote no encolado\nFin Recepción de Lote DE\nConsulta de Lote DE no realizada");
+
+        else resultado = resultado.concat("Fin Recepción de Lote DE\nConsulta de Lote DE\n");
+
+        RespuestaConsultaLoteDE respuestaConsulta = Sifen.consultaLoteDE(respuestaRecepcion.getdProtConsLote());
+
+        // Por cada Respuesta de Procesamiento de Lote
+        for (TgResProcLote gResProcLote : respuestaConsulta.getgResProcLoteList()) {
+
+            String sub = String.format("\t%s: %s\n",
+                    respuestaConsulta.getdCodResLot(), respuestaConsulta.getdMsgResLot());
+
+            // Por cada Respuesta de Procesamiento:
+            for (TgResProc gResProc : gResProcLote.getgResProc()) {
+                sub = sub.concat(String.format("\t* Cod %s: %s\n", gResProc.getdCodRes(), gResProc.getdMsgRes()));
+            }
+
+            resultado = resultado.concat(sub);
+        }
+
+        return resultado.concat("Fin Consulta de Lote DE");
+    }
+
+    /**
+     * Método que envía un Evento de Anulación al Sifen, y devuelve como resultado una cadena con el resultado
+     * de la Recepción del Envío.
+     * <p>
+     * Este método hace uso del método Sifen.recepcionEvento(eventosDE) de la librería RSHK-JSIFENLIB
+     *
+     * @param de El Documento Electrónico a anular
+     * @return Una cadena formateada con el resultado de la Recepción del Envío
+     * @throws SifenException Si la configuración de Sifen no fue establecida o, si algún dato necesario para la
+     *                        *                        consulta no pudo ser encontrado o, si la consulta no pudo ser realizada.
+     */
+    public static String pruebaAnularFactura(DocumentoElectronico de) throws SifenException {
+
+        EventosDE eventosDE = SifenExampleHelper.crearEventoAnulacion(de.obtenerCDC());
+        RespuestaRecepcionEvento respuesta = Sifen.recepcionEvento(eventosDE);
+
+        String resultado = String.format("Anulación de Factura:\n" +
+                        "\tCodigo de Estado: %s\n" +
+                        "\tRespuesta Recepción Evento:\n",
+                respuesta.getCodigoEstado());
+
+        //Por cada Respuesta de Procesamiento de Evento:
+        for (TgResProcEVe gResProcEVe : respuesta.getgResProcEVe()) {
+
+            String sub = String.format("\t\tID %s: %s\n", gResProcEVe.getId(), gResProcEVe.getdEstRes());
+
+            //Por cada Respuesta de Procesamiento:
+            for (TgResProc gResProc : gResProcEVe.getgResProc()) {
+                sub = sub.concat(String.format("\t\t* Cod %s: %s\n", gResProc.getdCodRes(), gResProc.getdMsgRes()));
+            }
+
+            resultado = resultado.concat(sub);
+        }
+        return resultado.concat("Fin Anulación de Factura");
+    }
+
+
+}
---
+++ src/main/java/com/roshka/sifen/SifenExampleHelper.java
@@ -0,0 +1,238 @@
+package com.roshka.sifen;
+
+import com.roshka.sifen.core.beans.DocumentoElectronico;
+import com.roshka.sifen.core.beans.EventosDE;
+import com.roshka.sifen.core.exceptions.SifenException;
+import com.roshka.sifen.core.fields.request.de.*;
+import com.roshka.sifen.core.fields.request.event.TgGroupTiEvt;
+import com.roshka.sifen.core.fields.request.event.TrGeVeCan;
+import com.roshka.sifen.core.fields.request.event.TrGesEve;
+import com.roshka.sifen.core.types.*;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SifenExampleHelper {
+
+    /**
+     * Método que genera y devuelve un Documento Electrónico. Este método se escribió con referencia al código
+     * del archivo SOAPTests.java
+     *
+     * @param dNumDoc Una cadena con el Número de Documento para la factura
+     * @return Un Documento Electrónico generado
+     */
+    private static DocumentoElectronico crearDE(String dNumDoc) {
+        LocalDateTime currentDate = LocalDateTime.now();
+
+        // Grupo A: Creación del Documento Electrónico
+        DocumentoElectronico DE = new DocumentoElectronico();
+        DE.setdFecFirma(currentDate);
+        DE.setdSisFact((short) 1);
+
+        // Grupo B: Campo de Operación del Documento Electrónico
+        TgOpeDE gOpeDE = new TgOpeDE();
+        gOpeDE.setiTipEmi(TTipEmi.NORMAL);
+        DE.setgOpeDE(gOpeDE);
+
+        // Grupo C: Campo del Timbrado del Documento Electrónico
+        TgTimb gTimb = new TgTimb();
+        gTimb.setiTiDE(TTiDE.FACTURA_ELECTRONICA);
+        gTimb.setdNumTim(12557605);
+        gTimb.setdEst("001");
+        gTimb.setdPunExp("002");
+        gTimb.setdNumDoc(dNumDoc);
+        gTimb.setdFeIniT(LocalDate.parse("2019-07-24"));
+        DE.setgTimb(gTimb);
+
+        // Grupo D: Datos Generales de la Operacion
+        TdDatGralOpe dDatGralOpe = new TdDatGralOpe();
+        dDatGralOpe.setdFeEmiDE(currentDate);
+
+        // D1: Campos inherentes a la Operación Comercial
+        TgOpeCom gOpeCom = new TgOpeCom();
+        gOpeCom.setiTipTra(TTipTra.PRESTACION_SERVICIOS);
+        gOpeCom.setiTImp(TTImp.IVA);
+        gOpeCom.setcMoneOpe(CMondT.PYG);
+        dDatGralOpe.setgOpeCom(gOpeCom);
+
+        // D2: Campos que identifican al Emisor
+        TgEmis gEmis = new TgEmis();
+        gEmis.setdRucEm("80089752");
+        gEmis.setdDVEmi("8");
+        gEmis.setiTipCont(TiTipCont.PERSONA_JURIDICA);
+        gEmis.setdNomEmi("DE generado en ambiente de prueba - sin valor comercial ni fiscal");
+        gEmis.setdDirEmi("Calle 1");
+        gEmis.setdNumCas("1234");
+        gEmis.setcDepEmi(TDepartamento.CAPITAL);
+        gEmis.setcCiuEmi(1);
+        gEmis.setdDesCiuEmi("ASUNCION (DISTRITO)");
+        gEmis.setdTelEmi("983169005");
+        gEmis.setdEmailE("gabrielgzb97@gmail.com");
+
+        // D2.1: Campos que describen la Actividad Economica del Emisor
+        List<TgActEco> gActEcoList = new ArrayList<>();
+
+        TgActEco gActEco = new TgActEco();
+        gActEco.setcActEco("62010");
+        gActEco.setdDesActEco("ACTIVIDADES DE PROGRAMACIÓN INFORMÁTICA");
+        gActEcoList.add(gActEco);
+
+        gEmis.setgActEcoList(gActEcoList);
+        dDatGralOpe.setgEmis(gEmis);
+
+        // D3: Campos que identifican al Receptor
+        TgDatRec gDatRec = new TgDatRec();
+        gDatRec.setiNatRec(TiNatRec.NO_CONTRIBUYENTE);
+        gDatRec.setiTiOpe(TiTiOpe.B2C);
+        gDatRec.setcPaisRec(PaisType.PRY);
+        gDatRec.setiTipIDRec(TiTipDocRec.CEDULA_PARAGUAYA);
+        gDatRec.setdNumIDRec("4184256");
+        gDatRec.setdNomRec("Gabriel Gomez");
+        dDatGralOpe.setgDatRec(gDatRec);
+        DE.setgDatGralOpe(dDatGralOpe);
+
+        // Grupo E: Campos específicos por tipo de Documento Electrónico
+        TgDtipDE gDtipDE = new TgDtipDE();
+
+        // E1: Campos que componen la Factura Electrónica
+        TgCamFE gCamFE = new TgCamFE();
+        gCamFE.setiIndPres(TiIndPres.OPERACION_ELECTRONICA);
+        gDtipDE.setgCamFE(gCamFE);
+
+        // E7: Campos que describen la condición de la operación
+        TgCamCond gCamCond = new TgCamCond();
+        gCamCond.setiCondOpe(TiCondOpe.CONTADO);
+
+        TgPaConEIni gPaConEIni = new TgPaConEIni();
+        gPaConEIni.setiTiPago(TiTiPago.EFECTIVO);
+        gPaConEIni.setdDesTiPag("Efectivo");
+        gPaConEIni.setdMonTiPag(BigDecimal.valueOf(1100000));
+        gPaConEIni.setcMoneTiPag(CMondT.PYG);
+
+        gCamCond.setgPaConEIniList(Collections.singletonList(gPaConEIni));
+        gDtipDE.setgCamCond(gCamCond);
+
+        // E8: Campos que describen los items de la operación
+        List<TgCamItem> gCamItemList = new ArrayList<>();
+
+        TgCamItem gCamItem = new TgCamItem();
+
+        gCamItem.setdCodInt("001");
+        gCamItem.setdDesProSer("Servicios Personales Prestados");
+        gCamItem.setcUniMed(TcUniMed.UNI);
+        gCamItem.setdCantProSer(BigDecimal.valueOf(1));
+
+        TgValorItem gValorItem = new TgValorItem();
+        gValorItem.setdPUniProSer(BigDecimal.valueOf(1000000));
+
+        TgValorRestaItem gValorRestaItem = new TgValorRestaItem();
+        gValorItem.setgValorRestaItem(gValorRestaItem);
+        gCamItem.setgValorItem(gValorItem);
+
+        TgCamIVA gCamIVA = new TgCamIVA();
+        gCamIVA.setiAfecIVA(TiAfecIVA.GRAVADO);
+        gCamIVA.setdPropIVA(BigDecimal.valueOf(100));
+        gCamIVA.setdTasaIVA(BigDecimal.valueOf(10));
+        gCamItem.setgCamIVA(gCamIVA);
+
+        gCamItemList.add(gCamItem);
+
+        gDtipDE.setgCamItemList(gCamItemList);
+        DE.setgDtipDE(gDtipDE);
+
+        // Grupo F: Campos que describen los subtotales y totales de la transacción documentada
+        DE.setgTotSub(new TgTotSub());
+
+        return DE;
+    }
+
+
+    /**
+     * Método para generar un Documento Electrónico y a su vez, generar el archivo xml correspondiente.
+     *
+     * @param rutaArchivo La ruta donde se guardará el archivo
+     * @param dNumDoc     El Número de Documento para su generación
+     * @return Un Documento Electrónico creado, y se genera el archivo xml correspondiente.
+     * @throws SifenException Una cadena formateada con el resultado de la Recepción y Consulta de Lote de DE realizadas (en el caso
+     *                        de la última, sólo si se pudo realizar la consulta)
+     */
+    private static DocumentoElectronico generarDE(Path rutaArchivo, String dNumDoc) throws SifenException {
+
+        DocumentoElectronico DE = crearDE(dNumDoc);
+        try {
+            Files.write(rutaArchivo, DE.generarXml().getBytes());
+            System.out.println("Archivo creado");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        return DE;
+    }
+
+    /**
+     * Método que carga o genera un Documento Electrónico con el Numero de Documento recibido.
+     * <p>
+     * Este método verífica si existe un archivo DE*******.xml en el directorio ../ donde * representa
+     * los dígitos de dNumDoc. Si existe, lo carga y lo retorna como DE.
+     * <p>
+     * Si no existe el archivo, genera el DE y guarda su xml en el directorio especificado.
+     *
+     * @param dNumDoc Número de Documento de la factura, debe ser una cadena de 7 dígitos.
+     * @return Un Documento Electrónico generado o cargado.
+     * @throws SifenException Una cadena formateada con el resultado de la Recepción y Consulta de Lote de DE realizadas (en el caso
+     *                        de la última, sólo si se pudo realizar la consulta)
+     */
+    public static DocumentoElectronico cargarDE(String dNumDoc) throws SifenException {
+        Path rutaArchivo = Paths.get(String.format("../DE%s.xml", dNumDoc));
+        DocumentoElectronico DE = null;
+        if (Files.exists(rutaArchivo)) {
+            try {
+                String xml = new String(Files.readAllBytes(rutaArchivo));
+                System.out.println("Factura cargada de archivo");
+                DE = new DocumentoElectronico(xml);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Creando factura\n");
+            DE = generarDE(rutaArchivo, dNumDoc);
+        }
+        return DE;
+    }
+
+    /**
+     * Método que crea un Evento de Anulación con el CDC de un DTE.
+     *
+     * @param cdc El CDC de un DTE a Anular.
+     * @return Un Evento de Anulación con el CDC de un DTE.
+     */
+    public static EventosDE crearEventoAnulacion(String cdc) {
+        LocalDateTime currentDate = LocalDateTime.now();
+
+        TrGeVeCan rGeVeCan = new TrGeVeCan();
+        rGeVeCan.setId(cdc);
+        rGeVeCan.setmOtEve("Prueba de cancelación de documento electrónico");
+
+        TgGroupTiEvt gGroupTiEvt = new TgGroupTiEvt();
+        gGroupTiEvt.setrGeVeCan(rGeVeCan);
+
+        TrGesEve rGesEve = new TrGesEve();
+        rGesEve.setId("1");
+        rGesEve.setdFecFirma(currentDate);
+        rGesEve.setgGroupTiEvt(gGroupTiEvt);
+
+        EventosDE eventosDE = new EventosDE();
+        eventosDE.setrGesEveList(Collections.singletonList(rGesEve));
+
+        return eventosDE;
+    }
+
+}
